8ba6c0e936eee02620a4938a88cff7589279c2f6
Diwakar <diwakar@gmail.com>
2022-07-26 17:37:59 +0530

    added test cases

--- test/errorAndSuccess.test.js
+++ test/errorAndSuccess.test.js
@@ -1,12 +1,28 @@
 import { html, fixture, expect } from '@open-wc/testing';
 // import { stub } from 'sinon';
+import Sinon from 'sinon';
+
 import '../src/SuccessAndError/Success.js';
 import '../src/SuccessAndError/Error.js';
 
-describe('Success screen ', () => {
+describe('Success screen ', async() => {
   // Write test cases inside this block
+
+ it('should show the success screen', async() => {
+  const el=await fixture(html`<loan-success></loan-success>`);
+  const myspy=Sinon.spy(el,'_toHome');
+   myspy();
+  expect(myspy.called).to.be.true;
+ })
 });
 
-describe('error screen', () => {
+describe('error screen', async() => {
   // Write test cases inside this block
+
+ it('should show the error screen', async () => {
+  const el=await fixture(html`<loan-error></loan-error>`);
+  const myspy=Sinon.spy(el,'_toHome');
+  myspy();
+  expect(myspy.called).to.be.true;
+ })
 });
--- test/loan-emi-details.test.js
+++ test/loan-emi-details.test.js
@@ -3,4 +3,32 @@ import '../src/LoanEMIDetails/LoanEMIDetails.js';
 
 describe('Loan EMI details', () => {
   // Write test cases inside this block
+
+  let el;
+  it("should display emi details", () => {
+    expect(el).to.be.accessible;
+  })
+
+  
+  let cancelBtn;
+  let continueBtn;
+  beforeEach(async () => {
+    const data = {
+      "interestRate": "7.00",
+      "monthlyEMI": "447.73",
+      "principal": "10000.00",
+      "interest": "745.42",
+      "totalAmount": "10745.42"
+    }
+    localStorage.setItem("type", "Home Loan");
+    localStorage.setItem("emi", JSON.stringify(data));
+
+    el = await fixture(html`<loanemi-details></loanemi-details>`);
+    cancelBtn = el.shadowRoot.querySelector("lion-button.cancel-btn")
+    continueBtn = el.shadowRoot.querySelector("lion-button.continue-btn")
+  })
+
+  afterEach(() => {
+    localStorage.removeItem("emi");
+  })
 });
